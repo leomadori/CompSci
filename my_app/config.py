d196a8197042f5ea7e2262357c8ad48cd51d2e13
leomadori <leonardoreina153@gmail.com>
2024-12-09 09:47:02 +0100

    Update config.py

--- my_app/config.py
+++ my_app/config.py
@@ -1,5 +1,5 @@
 SPOTIPY_CLIENT_ID = '53074fbca7704aef8ff5e43f4371bca2'
-SPOTIPY_CLIENT_SECRET = '30a69a1e644640168c85054e1c53b0c3'
+SPOTIPY_CLIENT_SECRET = 'efd9415d3ddd4e0ba1260d139e938d77'
 SPOTIPY_REDIRECT_URI = 'http://localhost:8505'
 
 scope = "user-top-read playlist-modify-public playlist-modify-private"
